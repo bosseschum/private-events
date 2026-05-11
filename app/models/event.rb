69ba077c2ae363bb042c10746a836992a69ce1be
bosseschum <accounts@bschum.de>
2026-05-11 17:28:28 +0200

    tweak models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
+  has_many :registrations, class_name: "Registration", foreign_key: "event_id", dependent: :destroy
+  has_many :attendees, through: :registrations, source: :attendee
+
   belongs_to :creator, class_name: "User"
 
   validates :title, :description, :date, :location, :creator, presence: true
-  validates :registrations, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :registrations, foreign_key: 'attendee_id'
-  has_many :hosted_events, class_name: 'Event', foreign_key: 'creator_id'
+  has_many :registrations, foreign_key: "attendee_id", dependent: :destroy
+  has_many :hosted_events, class_name: "Event", foreign_key: "creator_id", dependent: :destroy
   has_many :attended_events, through: :registrations, source: :event
 
   # Include default devise modules. Others available are:
