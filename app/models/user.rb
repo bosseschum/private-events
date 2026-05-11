c5f8579a2bbda697fabb054d20c34cf1cfe5675b
bosseschum <accounts@bschum.de>
2026-05-11 16:49:06 +0200

    Setup routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :registrations, foreign_key: 'attendee_id'
+  has_many :hosted_events, class_name: 'Event', foreign_key: 'creator_id'
+  has_many :attended_events, through: :registrations, source: :event
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
