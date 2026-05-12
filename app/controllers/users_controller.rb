697b8d40dcca1018bd060c255620dfe4e48071b5
bosseschum <accounts@bschum.de>
2026-05-12 16:41:36 +0200

    fixed naming inconsistencies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def show
-        @user = current_user
+        @user = User.find(params[:id])
     end
 
     def new
@@ -38,6 +38,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password)
+        params.require(:user).permit(:username, :email, :password)
     end
 end
