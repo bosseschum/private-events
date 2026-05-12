64ea003f43fdc6a4b5a3eff2a37ffb0e830261c1
bosseschum <accounts@bschum.de>
2026-05-12 14:49:16 +0200

    finish events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,13 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :set_event, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find(params[:id])
+    set_event
     @registration = Registration.find_by(attendee: current_user, event: @event)
   end
 
@@ -24,7 +25,30 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+    set_event
+  end
+
+  def update
+    set_event
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    set_event
+    @event.destroy
+    redirect_to events_path
+  end
+
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:title, :description, :date, :location)
